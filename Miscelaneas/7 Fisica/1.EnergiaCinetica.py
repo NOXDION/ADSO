afd344fc036c5dd1dfcaffe6e02538d68a39f7dc
NOXDION <diesmarsw@gmail.com>
2022-12-08 22:53:45 -0500

    fisica

---
+++ Miscelaneas/7 Fisica/1.EnergiaCinetica.py
@@ -0,0 +1,14 @@
+#Energia cinetica Ec=(m*(v)**2)/2
+print('1.En un juego de bolos, Pedro lanza una bola de boliche de 7500 g '
+'a una velocidad de 8 m/s. Encuentre la energía cinética de la bola de boliche.')
+
+'''m=float(7.5)
+v=float(8)
+Ec=(m*(v)**2)/2'''
+
+print('')
+m=float(input('Ingrese la masa en kg: '))
+v=float(input('Ingrese la velocidad en m/s: '))
+Ec=(m*(v)**2)/2
+print('')
+print('La energía cinética de la bola de boliche es de',Ec,'Joules')
