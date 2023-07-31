8fe117cde876e5405209175f0fa35a73f99b3f8b
NOXDION <diesmarsw@gmail.com>
2023-07-30 23:16:25 -0500

    30/07/2023

--- 4T/Sql/TrabajoFormativo.sql
+++ 4T/Sql/TrabajoFormativo.sql
@@ -1,9 +1,9 @@
 CREATE DATABASE trabajoformativo;
 
-CREATE TABLE empleado (nroDoc VARCHAR(14) PRIMARY KEY,
-                    tipoDoc VARCHAR(22),
+CREATE TABLE empleado (nroDoc VARCHAR(10) PRIMARY KEY,
+                    tipoDoc VARCHAR(2),
                     nombres VARCHAR(40),
-                    apellidos VARCHAR(60),
+                    apellidos VARCHAR(40),
                     genero VARCHAR(10),
                     sueldo FLOAT,
                     horaTrabajada TINYINT,
@@ -14,39 +14,44 @@ CREATE TABLE empleado (nroDoc VARCHAR(14) PRIMARY KEY,
 DELIMITER //
 CREATE TRIGGER insertar_sueldo BEFORE INSERT ON empleado FOR EACH ROW
 BEGIN
-    IF NEW.sueldo < 0 THEN SET NEW.sueldo = 1160000;
-    ELSEIF NEW.sueldo > 10000000 THEN SET NEW.sueldo = 5000000;
+    IF NEW.sueldo < 0 THEN
+    SET NEW.sueldo = 1160000;
+    ELSEIF NEW.sueldo > 10000000 THEN
+    SET NEW.sueldo = 5000000;
     END IF;
 END//
 DELIMITER ;
 
-INSERT INTO empleado VALUES('101','C.C','Mateo','Martinez','M',1200000,4,'Desarollador');
-INSERT INTO empleado VALUES('201','C.C','Michell','Gomez','F',4000000,2,'Desarolladora');
+INSERT INTO empleado VALUES('1028442001','CC','Mateo','Martinez','M',1200000,4,'Desarollador');
+INSERT INTO empleado VALUES('1028442002','CC','Michell','Gomez','F',4000000,2,'Desarolladora');
 
 SELECT * FROM empleado;
 
-INSERT INTO empleado VALUES('301','C.C','Juan','Ruiz','M',-1000000,8,'Desarollador');
-INSERT INTO empleado VALUES('401','C.C','Laura','Fernandez','F',11000000,6,'Desarolladora');
+INSERT INTO empleado VALUES('1028442003','CC','Juan','Ruiz','M',-1000000,8,'Desarollador');
+INSERT INTO empleado VALUES('1028442004','CC','Laura','Fernandez','F',11000000,6,'Desarolladora');
 
 SELECT * FROM empleado;
 
+----
 
 DELIMITER //
 CREATE TRIGGER actualizar_sueldo BEFORE UPDATE ON empleado FOR EACH ROW
 BEGIN
-    IF NEW.sueldo < 0 THEN SET NEW.sueldo = 1160000;
-    ELSEIF NEW.sueldo > 10000000 THEN SET NEW.sueldo = 5000000;
+    IF NEW.sueldo < 0 THEN
+    SET NEW.sueldo = 1160000;
+    ELSEIF NEW.sueldo > 10000000 THEN
+    SET NEW.sueldo = 5000000;
     END IF;
 END//
 DELIMITER ;
 
-UPDATE empleado SET sueldo=1400000 WHERE nroDoc = '101';
-UPDATE empleado SET sueldo=8000000 WHERE nroDoc = '201';
+UPDATE empleado SET sueldo=1400000 WHERE nroDoc = '1028442001';
+UPDATE empleado SET sueldo=8000000 WHERE nroDoc = '1028442002';
 
 SELECT * FROM empleado;
 
-UPDATE empleado SET sueldo=-4500 WHERE nroDoc = '101';
-UPDATE empleado SET sueldo=11000000 WHERE nroDoc = '201';
+UPDATE empleado SET sueldo=-4500 WHERE nroDoc = '1028442001';
+UPDATE empleado SET sueldo=11000000 WHERE nroDoc = '1028442002';
 
 SELECT * FROM empleado;
 
@@ -55,8 +60,9 @@ SELECT * FROM empleado;
 DELIMITER |
 CREATE TRIGGER insertar_sueldo BEFORE INSERT ON empleado FOR EACH ROW
 BEGIN
-    CASE 
-    WHEN NEW.sueldo < 0 THEN SET NEW.sueldo = 1160000;
+    CASE
+    WHEN NEW.sueldo < 0 THEN
+    SET NEW.sueldo = 1160000;
     WHEN NEW.sueldo > 5000001 THEN SET NEW.sueldo = 5000000;
     END CASE;
 END;
