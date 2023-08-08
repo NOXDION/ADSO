417b54e9f16c96bd0bf17557bf0e0f77c5f7393d
NOXDION <diesmarsw@gmail.com>
2023-08-07 21:16:18 -0500

    Java con bd

---
+++ 5T/JDK/ConexionBd/src/conexionbd/Conectar.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package conexionbd;
+
+/**
+ *
+ * @author SENA
+ */
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+
+public class Conectar {
+    Connection conectar = null;
+    
+    public Connection conexion(){
+        try{
+            Class.forName("com.mysql.jdbc.Driver");   
+
+            conectar = DriverManager.getConnection("jdbc:mysql://localhost/techinn", "root","");
+        }
+        catch(Exception e){
+            System.out.print(e.getMessage());
+        }
+        
+        return conectar;
+        
+    }
+    
+}
